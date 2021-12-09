cac7d7e3375c49907511e5495e1f08cd94e51b26
Inventor Dave <digital.artist@hotmail.co.uk>
2021-12-09 12:00:18 +0000

    This should be an adequate primer.
    I recommend that if this guide was useful to you, that you consider reading a lot more than you do. Not just google, then read an answer, really deep-dive into a specific issue, and take notes. Then, re-write them, for memory retention purposes.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ HRESULT hr;
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)	{
 	
-	printf( "Select a file in the FileOpen dialog box, to see the path to the chosen file returned.\n" );
+	printf( "Select a file in the FileOpen/FileSave dialog box, to see the path to the chosen file returned.\n" );
 	
 	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
 
@@ -20,12 +20,12 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	
 	if (SUCCEEDED(hr))	{
 		
-		IFileOpenDialog *pFileOpen;
+		IFileOpenDialog *pFileOpen; // Change IFileOpenDialog to IFileSaveDialog for a "Save File" Box. 
 		
-		hr = CoCreateInstance(	CLSID_FileOpenDialog,
+		hr = CoCreateInstance(	CLSID_FileOpenDialog, // Likewise, change to CLSID_FileSaveDialog.
 							NULL,
 							CLSCTX_ALL, 
-							IID_IFileOpenDialog,
+							IID_IFileOpenDialog, // Likewise, change to IID_IFileSaveDialog.
 							reinterpret_cast<void**>(&pFileOpen)
 						);
 		
@@ -43,18 +43,14 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 				hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
 
 				// Display the file name to the user.
-				if (SUCCEEDED(hr))	{
-					
+				if (SUCCEEDED(hr))
+					MessageBoxW(NULL, pszFilePath, 	L"Selected File.", MB_OK);
 					/*
 						Note:	When using IFileSaveDialog, hr is only SUCCESS when you select an existing
 								file to overwrite (which won't be overwritten - it's a read operation, not a write operation),
 								otherwise it'll return the equivalent of FALSE, meaning this MsgBox will not appear.
 								When using IFileOpenDialog, hr is SUCCESS upon the user selecting a file to open.
 					*/
-					
-					MessageBoxW(NULL, pszFilePath, 	L"Selected File.", MB_OK);
-					//CoTaskMemFree(pszFilePath);
-				}
 			}
 			
 			pItem->Release();
@@ -65,7 +61,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		
 	CoUninitialize();
 	
-	printf( "Path & File returned: '%S'\n", pszFilePath ); // Note, it is %S, not %s, as %S tells printf the param string is a wchar string.
+	printf( "Path & File returned: '%S'\n", pszFilePath ); // Note, it is %S, not %s, as %S tells printf the param string is a widechar string.
 
 	return 0;
 }
